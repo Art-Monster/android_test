e9feb4e4e1a9cd6dc0faec46db3ea7f20b8fd690
Art-Monster <at1979@bk.ru>
2022-07-15 04:51:38 +0300

    android_test

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -2,10 +2,33 @@ package com.sirius.test_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerViewReviews = findViewById<RecyclerView>(R.id.Reviews)
+        recyclerViewReviews.layoutManager = LinearLayoutManager(this)
+
+        val reviewList = mutableListOf(
+            ReviewModel(
+                userImage = "https://ibb.co/WcJMjSw",
+                userName = "Auguste Conte",
+                date = "February 14, 2019",
+                message = "Once you start to learn its secrets, there’s a wild and exciting variety of play here that’s unmatched, even by its peers."
+            ),
+            ReviewModel(
+                userImage = "https://ibb.co/p1q4QZr",
+                userName = "Jang Marcelino",
+                date = "February 14, 2019",
+                message =
+                "Once you start to learn its secrets, there’s a wild and exciting variety of play here that’s unmatched, even by its peers."
+            )
+        )
+        val adapter = ReviewsAdapter(reviewList)
+        recyclerViewReviews.adapter = adapter
+
     }
 }
